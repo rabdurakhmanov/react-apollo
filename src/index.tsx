fa27a113fd92c4250ea2fac2551dceeaa2cea00c
rabdurakhmanov <rabdurakhmanov@icloud.com>
2022-01-03 19:47:06 +0300

    Refactor

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 const client = new ApolloClient({
   uri: 'https://48p1r2roz4.sse.codesandbox.io',
@@ -20,8 +19,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
